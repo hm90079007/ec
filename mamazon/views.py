1392d4f0abe3a981f2f2635172022192496e3dbc
hayato <hm90079007@gmail.com>
2020-04-19 23:44:14 +0900

    5

--- mamazon/views.py
+++ mamazon/views.py
@@ -1,5 +1,19 @@
-from django.views.generic import TemplateView
+#データをテンプレート(htmlとか）)に供給する役目
 
+from django.views.generic import TemplateView, ListView
+from .models import Product
 
 class Home(TemplateView):
     template_name = 'mamazon/home.html'
+
+class ProductListView(ListView):
+    model = Product
+    template_name ='mamazon/list.html'
+#    context_object_name = "products"
+
+    def get_queryset(self):
+        queryset=Product.objects.all()
+        if 'query' in self.request.GET:
+            qs=self.request.GET['query']
+            queryset = queryset.filter(name__contains=qs)
+        return queryset
