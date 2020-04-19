d0bab69d299ecf0ad6a4b81d53cbc88222b10791
hayato <hm90079007@gmail.com>
2020-04-19 17:01:27 +0900

    4

--- mamazon/models.py
+++ mamazon/models.py
@@ -5,3 +5,8 @@ class Product(models.Model):
     price = models.IntegerField()
     description = models.TextField()
     thumbnail = models.ImageField(upload_to='thumbnails/ ')
+
+    def __str__(self):
+        return self.name
+        ##インスタンスを生成した際にまず自動的に呼び出すメソッドの事です。
+        ##クラスの初期設定などを行うためのメソッドになります。
