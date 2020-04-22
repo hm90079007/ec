ee36972c394022cac3c1a9a957ac0f6d38cfb57b
hayato <hm90079007@gmail.com>
2020-04-22 18:32:32 +0900

    8

--- cart/models.py
+++ cart/models.py
@@ -4,9 +4,11 @@ from mamazon.models import Product
 
 User = settings.AUTH_USER_MODEL
 
+
 class Cart(models.Model):
-    user =  models.Foreignkey(user,null=True,blank=True,on_delete=models.CASCADE) #Many to one たくさんのカートそれぞれに一つのユーザー. nullはDB上の値、blankは登録フォーム上の値
+#    context_object_name = 'carts_list'#臨時
+    user =  models.ForeignKey(User,null=True,blank=True,on_delete=models.CASCADE) #Many to one たくさんのカートそれぞれに一つのユーザー. nullはDB上の値、blankは登録フォーム上の値
     products = models.ManyToManyField(Product, blank=True) #　Many to Many たくさんのカートそれぞれにたくさんの商品
     total = models.DecimalField(default=0.00, max_digits=9, decimal_places=2) #小数点OKフィールド（詳細設定）
-    created =　models.DateTimeField(auto_now_add=True)　#作られた時間だけ
+    created = models.DateTimeField(auto_now_add=True)#作られた時間だけ
     updated = models.DateTimeField(auto_now=True) #更新された時間も表示する
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from .models import Cart
+from django.views.generic import TemplateView, ListView, DetailView
 
-# Create your views here.
+class CartDetailView(DetailView):
+    model = Cart
+    template_name = 'cart/shopping_cart.html'
